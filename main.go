58d01304b439b6026340358b6cce9d7c24909e6b
Jon Wear <jon@dramafever.com>
2015-07-17 18:13:42 -0400

    update names & region

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ import (
 	"github.com/AdRoll/goamz/sqs"
 )
 
-//QuestionQName : name of the questions SQS Queue
-const QuestionQName = "demo-questions"
+//ProblemQName : name of the problems SQS Queue
+const ProblemQName = "demo-problems"
+
+//ProblemSharedQName : name of the problem shard SQS Queue
+const ProblemSharedQName = "demo-problem-shard"
 
 //AnswerQName : name of the answers SQS Queue
 const AnswerQName = "demo-answers"
@@ -20,17 +23,13 @@ const AnswerQName = "demo-answers"
 //SQS instance of the sqs var from goamz
 var SQS *sqs.SQS
 
-//Question : struct that holds the data for the math problem.
-type Question struct {
+//Problem : struct that holds the data for the math problem.
+type Problem struct {
 	Num1 int
 	Num2 int
 }
 
-var bufferCount = 50 //Playing with this get's different speep through puts.
-//The mbAir needs it around 50 or it starts erroring out.
-//The mbPro can handle higher values, but diminishing returns.
-
-//But, even set at 50, the mbAir can put 100,000 items in the queu in  48 seconds.
+var bufferCount = 200 //Playing with this get's different speep through puts.
 
 var sem = make(chan bool, bufferCount)
 
@@ -41,26 +40,26 @@ func doRun(public string, secret string, maxworkers int) error {
 
 	auth := aws.Auth{AccessKey: public, SecretKey: secret}
 	region := aws.Region{}
-	region.Name = "us-west-2"
-	region.SQSEndpoint = "http://sqs.us-west-2.amazonaws.com"
+	region.Name = "us-east-1"
+	region.SQSEndpoint = "http://sqs.us-east-1.amazonaws.com"
 	SQS = sqs.New(auth, region)
 	if SQS == nil {
 		return fmt.Errorf("Can't get sqs reference for %v %v", auth, region)
 	}
-	questionq, getErr := SQS.GetQueue(QuestionQName)
+	problemq, getErr := SQS.GetQueue(ProblemQName)
 	if getErr != nil {
-		fmt.Println(QuestionQName)
+		fmt.Println(ProblemQName)
 		return getErr
 	}
 	msgSlice := make([]sqs.Message, 0, 10) //A slice that holds up to 10 messages
 	msgAll := [][]sqs.Message{}
 
-	for i := 0; i < 100000; i++ {
+	for i := 0; i < 1000; i++ {
 		num1 := rand.Intn(9) + 1
 		num2 := rand.Intn(9 + 1)
-		q := Question{num1, num2}
-		jsonQ, _ := json.Marshal(&q)
-		msg := sqs.Message{Body: base64.StdEncoding.EncodeToString(jsonQ)}
+		p := Problem{num1, num2}
+		jsonP, _ := json.Marshal(&p)
+		msg := sqs.Message{Body: base64.StdEncoding.EncodeToString(jsonP)}
 		msgSlice = append(msgSlice, msg)
 		if len(msgSlice) == 10 {
 			msgAll = append(msgAll, msgSlice)
@@ -68,10 +67,9 @@ func doRun(public string, secret string, maxworkers int) error {
 		}
 	}
 	for _, s := range msgAll {
-		s := s //It's idomatic go I swear! http://golang.org/doc/effective_go.html#channels
 		sem <- true
 		go func(sl10 []sqs.Message) {
-			addToQuestionQ(sl10, *questionq)
+			addToProblemQ(sl10, *problemq)
 			defer func() { <-sem }()
 		}(s)
 	}
@@ -81,7 +79,7 @@ func doRun(public string, secret string, maxworkers int) error {
 	return nil
 }
 
-func addToQuestionQ(msgList []sqs.Message, sqsQ sqs.Queue) {
+func addToProblemQ(msgList []sqs.Message, sqsQ sqs.Queue) {
 	_, respErr := sqsQ.SendMessageBatch(msgList)
 	if respErr != nil {
 		log.Println("ERROR:", respErr)
